7ae1b9b02a60c43401d8a86c65866a1c6271be91
admin <2032214984@qq.com>
2022-09-09 23:28:12 +0800

    9.09练习

--- mybatis-06/src/main/java/feng/dao/StudentMapper.java
+++ mybatis-06/src/main/java/feng/dao/StudentMapper.java
@@ -4,9 +4,12 @@ import feng.pojo.Student;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 /**
  * Created by 小冯 on 2022/9/7 21:05
  */
 public interface StudentMapper {
     //查询所有的学生的信息以及对应的老师的信息
+    public List<Student> getStudent();
 }
--- mybatis-06/src/test/java/MyTest.java
+++ mybatis-06/src/test/java/MyTest.java
@@ -1,7 +1,12 @@
+import feng.dao.StudentMapper;
 import feng.dao.TeacherMapper;
+import feng.pojo.Student;
 import feng.pojo.Teacher;
 import feng.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by 小冯 on 2022/9/7 21:13
@@ -12,6 +17,16 @@ public class MyTest {
         TeacherMapper mapper = sqlSession.getMapper(TeacherMapper.class);
         Teacher teacher = mapper.getTeacher(1);
         System.out.println(teacher);
+        sqlSession.close();
+    }
+    @Test
+    public void testStudent(){
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> student = mapper.getStudent();
+        for (Student student1 : student) {
+            System.out.println(student1);
+        }
         sqlSession.close();
     }
 }
